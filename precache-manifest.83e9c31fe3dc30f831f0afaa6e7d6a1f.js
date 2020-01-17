240fc67ecfe653cdd3c29229efd8e81d9d46f1cb
artrugh <arturo.rugh@gmail.com>
2020-01-17 20:03:35 +0100

    Updates

---
+++ precache-manifest.83e9c31fe3dc30f831f0afaa6e7d6a1f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e669d2e4758a973ebb1cc040f46f99",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce9c5cc5326e178934db",
+    "url": "/portfolio/static/css/2.67eb4ace.chunk.css"
+  },
+  {
+    "revision": "00f6cf6bce9fd621d7bb",
+    "url": "/portfolio/static/css/main.8ec4c82e.chunk.css"
+  },
+  {
+    "revision": "ce9c5cc5326e178934db",
+    "url": "/portfolio/static/js/2.042c7659.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.042c7659.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00f6cf6bce9fd621d7bb",
+    "url": "/portfolio/static/js/main.0da475b8.chunk.js"
+  },
+  {
+    "revision": "255a19e524ffeb5febac",
+    "url": "/portfolio/static/js/runtime-main.5cec9bc0.js"
+  },
+  {
+    "revision": "eb40abbe9ee2a9037338e3680293d985",
+    "url": "/portfolio/static/media/banca0.eb40abbe.png"
+  },
+  {
+    "revision": "806d5835a0b80343190300e64fe650c3",
+    "url": "/portfolio/static/media/banca1.806d5835.png"
+  },
+  {
+    "revision": "f75c2edbe4dd160a09e7ffaed2bb2064",
+    "url": "/portfolio/static/media/banca2.f75c2edb.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/portfolio/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/portfolio/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/portfolio/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/portfolio/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/portfolio/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/portfolio/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/portfolio/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/portfolio/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/portfolio/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/portfolio/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/portfolio/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/portfolio/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/portfolio/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/portfolio/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/portfolio/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "54b6d289b99410250c32c11598a6b9a6",
+    "url": "/portfolio/static/media/logo_black.54b6d289.svg"
+  },
+  {
+    "revision": "5820fad0b85684ca9c91a15a62504111",
+    "url": "/portfolio/static/media/logo_mask_white.5820fad0.svg"
+  },
+  {
+    "revision": "c38ea6f864a42be091812916d698929f",
+    "url": "/portfolio/static/media/logo_white.c38ea6f8.svg"
+  },
+  {
+    "revision": "828bcf7e21ba3c6050f2e75806456154",
+    "url": "/portfolio/static/media/personal0.828bcf7e.png"
+  },
+  {
+    "revision": "0fc5f48520e672e895891444f73f5fd5",
+    "url": "/portfolio/static/media/personal1.0fc5f485.png"
+  },
+  {
+    "revision": "e29e52993b76b702e50e625b8baf93f4",
+    "url": "/portfolio/static/media/subiendo0.e29e5299.png"
+  },
+  {
+    "revision": "bc5fd0a9e9faade220ee25a22a2e3174",
+    "url": "/portfolio/static/media/subiendo_logo.bc5fd0a9.svg"
+  },
+  {
+    "revision": "837b970d5c77507b2ea4bb0ec18059a0",
+    "url": "/portfolio/static/media/webpack-icon.837b970d.svg"
+  },
+  {
+    "revision": "64a4459e09ac7b033a7d1ca52cd477df",
+    "url": "/portfolio/static/media/x.64a4459e.svg"
+  }
+]);
