89ecdee1d349af87cc55a9e4c6968dd10859736f
artrugh <arturo.rugh@gmail.com>
2020-01-26 11:31:25 +0100

    mediaquery about-contact

--- src/components/LateralBar.js
+++ src/components/LateralBar.js
@@ -5,7 +5,7 @@ import uuid from 'uuid';
 const LateralBar = props => {
     const LateralBar = () => {
         let numbers = [];
-        for (let i = 0; i < (props.screen / 25); i += 1) {
+        for (let i = 0; i < (props.screen / 30); i += 1) {
             numbers.push(
                 <div key={uuid.v4()} >
                     <div className="spot">
